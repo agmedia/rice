2631824095d450e1a9eacb4dfe23bd0bdc90bf93
tomashina <tomislav@agmedia.hr>
2024-05-09 10:17:10 +0200

    if action product blade

--- resources/views/front/catalog/category/product.blade.php
+++ resources/views/front/catalog/category/product.blade.php
@@ -26,7 +26,7 @@
 
             <h3 class="product-title fs-sm text-truncate"><a href="{{ url($product->url) }}">{{ $product->name }}</a></h3>
             {!! $product->category_string !!}
-            @if ($product->main_price > $product->main_special and !$product->action->min_cart)
+            @if ($product->main_price > $product->main_special  and $product->action and !$product->action->min_cart)
                 <div class="product-price"><small><span class="text-muted">{{ __('front/ricekakis.nc_30') }}: {{ $product->main_price_text }}  @if($product->secondary_price_text){{ $product->secondary_price_text }} @endif</span></small></div>
                 <div class="product-price text-red"><span class="text-red fs-md">{{ $product->main_special_text }} @if($product->secondary_special_text) <small class="text-muted">{{ $product->secondary_special_text }}</small> @endif</span></div>
             @else
