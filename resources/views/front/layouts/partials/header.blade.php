43ce9c76fcff28ec28a25d8f9b3a312715e082ad
tomashina <tomislav@agmedia.hr>
2024-06-08 07:59:28 +0200

    login fix

--- resources/views/front/layouts/partials/header.blade.php
+++ resources/views/front/layouts/partials/header.blade.php
@@ -84,11 +84,7 @@
                 <div class="widget widget-categories">
                     <div class="accordion" id="shop-menu">
                         <!-- Homepages-->
-                      {{--   @foreach ($uvjeti_kupnje->sortBy('title') as $page)
-                            <div class="accordion-item border-bottom">
-                                <h3 class="accordion-header px-grid-gutter"><a class="nav-link-style d-block fs-md fw-normal py-3" href="{{ route('catalog.route.page', ['page' => $page]) }}"><span class="d-flex align-items-center">{{ $page->title }}</span></a></h3>
-                            </div>
-                        @endforeach--}}
+
 
 
                         @if (isset($pages) && $pages)
