6b75355a6c8b05afe2682e4080660ef0846cceb4
tomashina <tomislav@agmedia.hr>
2024-06-13 08:40:01 +0200

    fix blog widget

--- resources/views/front/layouts/widget/widget_page_carousel.blade.php
+++ resources/views/front/layouts/widget/widget_page_carousel.blade.php
@@ -102,10 +102,10 @@
                 @foreach ($data['items'] as $item)
                     <!-- Product-->
                     <div>
-                        <div class="card"><a class="blog-entry-thumb" href="{{ route('catalog.route.blog', ['blog' => $item]) }}"><img class="card-img-top" load="lazy" src="{{ $item['image'] }}" width="400" height="230" alt="{{ $item['title'] }}"></a>
+                        <div class="card"><a class="blog-entry-thumb" href="{{ route('catalog.route.blog', ['blog' => $item]) }}/{{ $item->translation->slug }}"><img class="card-img-top" load="lazy" src="{{ $item['image'] }}" width="400" height="230" alt="{{ $item['title'] }}"></a>
                             <div class="card-body">
-                                <h2 class="h6 blog-entry-title"><a href="{{ route('catalog.route.blog', ['blog' => $item]) }}">{{ $item['title'] }}</a></h2>
-                                <p class="fs-sm">{{ $item['short_description'] }}</p>
+                                <h2 class="h6 blog-entry-title"><a href="{{ route('catalog.route.blog', ['blog' => $item]) }}/{{ $item->translation->slug }}">{{ $item['title'] }}</a></h2>
+                                <p class="fs-sm">{{ $item->translation['short_description'] }}</p>
                                 <div class="fs-xs text-nowrap"><a class="blog-entry-meta-link text-nowrap" href="#">{{ \Carbon\Carbon::make($item['created_at'])->locale('hr')->format('d.m.Y.') }}</a></div>
                             </div>
                         </div>
