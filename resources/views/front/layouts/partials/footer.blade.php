16c7c2e4de1f00b984d2074e666ce715f0c6d753
tomashina <tomislav@agmedia.hr>
2024-07-11 12:15:18 +0200

    widget

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -76,7 +76,7 @@
                                 <li  class="widget-list-item"> <span class="widget-list-link">IBAN: HR9223400091111126783</span></li>
                             </ul>
                         </div>
-                      {{--  <div class="review-widget_net" data-uuid="9ba05add-2ec4-47e0-9d98-72aec4875742" data-template="10" data-lang="en" data-theme="light"></div>--}}
+                       <div class="review-widget_net" data-uuid="9ba05add-2ec4-47e0-9d98-72aec4875742" data-template="10" data-lang="en" data-theme="light"></div>
 
                     </div>
                     <div class="col-lg-4 mb-lg-0 mb-4">
