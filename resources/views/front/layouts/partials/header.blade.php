63ce9702c63bba025b1f252666d9731949c6e371
Filip Jankoski <filip@agmedia.hr>
2024-10-26 11:13:48 +0200

    _fix: translation page error.

--- resources/views/front/layouts/partials/header.blade.php
+++ resources/views/front/layouts/partials/header.blade.php
@@ -84,19 +84,14 @@
                 <div class="widget widget-categories">
                     <div class="accordion" id="shop-menu">
                         <!-- Homepages-->
-
-
-
                         @if (isset($pages) && $pages)
                             @foreach($pages as $page)
-
-                                @if($page->translation->title !='Homepage' and $page->group =='page' )
-
-                                    <div class="accordion-item border-bottom">
-
-                                        <h3 class="accordion-header px-grid-gutter"><a class="nav-link-style d-block fs-md fw-normal py-3" href="{{ current_locale() }}/info/{{ $page->translation->slug }}"><span class="d-flex align-items-center">{{ $page->translation->title }}</span></a></h3>
-                                    </div>
-
+                                @if (isset($page->translation->title))
+                                    @if($page->translation->title != 'Homepage' and $page->group =='page' )
+                                        <div class="accordion-item border-bottom">
+                                            <h3 class="accordion-header px-grid-gutter"><a class="nav-link-style d-block fs-md fw-normal py-3" href="{{ current_locale() }}/info/{{ $page->translation->slug }}"><span class="d-flex align-items-center">{{ $page->translation->title }}</span></a></h3>
+                                        </div>
+                                    @endif
                                 @endif
                             @endforeach
                         @endif
