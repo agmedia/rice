f69c50d5ed529b957bc327ef616bff34dffac814
Filip Jankoski <filip@agmedia.hr>
2025-06-03 09:51:29 +0200

    _fix: SEO x-default .

--- resources/views/front/layouts/partials/hreflang.blade.php
+++ resources/views/front/layouts/partials/hreflang.blade.php
@@ -3,8 +3,8 @@
         <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang['code'], $lang['slug'], [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
     @endforeach
 @else
-@foreach (ag_lang() as $lang )
-@if (isset($page) && $page->id == 5 )
+    @foreach (ag_lang() as $lang )
+        @if (isset($page) && $page->id == 5 )
             <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('index'), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
         @endif
         @if (isset($blog) && !$frontblogs )
@@ -22,29 +22,32 @@
         @if (isset($brand)  )
 
 
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route.brand', ['brand' => $brand->translation->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+            <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route.brand', ['brand' => $brand->translation->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
 
 
         @endif
-@if (isset($page) && $page->id != 5)
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route.page', ['page' => $page->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
-@endif
-@if (isset($group) && isset($cat) && ! $cat)
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path'))]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
-@endif
-@if (isset($cat) && $cat && ! $subcat && ! $prod)
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
- @endif
- @if (isset($subcat) && $subcat && ! $prod)
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug, 'subcat' => $subcat->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
-@endif
- @if (isset($prod) && $prod)
- @if (isset($cat) && $cat && ! $subcat)
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug, 'subcat' => $prod->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
-@else
-<link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug, 'subcat' => $prod->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
-@endif
-@endif
-@endforeach
+        @if (isset($page) && $page->id != 5)
+            <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route.page', ['page' => $page->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+        @endif
+        @if (isset($group) && isset($cat) && ! $cat)
+            <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path'))]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+        @endif
+        @if (isset($cat) && $cat && ! $subcat && ! $prod)
+            <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug])) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+            @if ($lang->code == 'hr')
+                <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug])) }}" hreflang="x-default" />
+            @endif
+        @endif
+        @if (isset($subcat) && $subcat && ! $prod)
+            <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug, 'subcat' => $subcat->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+        @endif
+        @if (isset($prod) && $prod)
+            @if (isset($cat) && $cat && ! $subcat)
+                <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug, 'subcat' => $prod->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+            @else
+                <link rel="alternate" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($lang->code, route('catalog.route', ['group' => \Illuminate\Support\Str::slug(config('settings.group_path')), 'cat' => $cat->translation($lang->code)->slug, 'subcat' => $prod->translation($lang->code)->slug]), [], true) }}" hreflang="{{ Str::lower($lang->code) }}-HR" />
+            @endif
+        @endif
+    @endforeach
 @endif
 
